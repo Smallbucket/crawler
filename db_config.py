561629dc5b93ab2b6ebb2cb4c82b9008d009e956
tourDJ <tangxiaojun_123@126.com>
2019-04-24 22:38:04 +0800

    fixed train station function

--- db_config.py
+++ db_config.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 # data path
-data_root_path = '/home/tang/python/python-crawler/'
+data_root_path = '/home/ivan/python/crawler'
 
 # mongodb account
 user="tang"
 pwd="123456"
-server="localhost"
+server="127.0.0.1"
 port="27107"
 db_name="tangdb"
---
+++ train_number.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import requests
+import bs4
+import json
+import main
+
+resources_file_path = '/resources/train/trainNameList.ini'
+scratch_url = 'http://www.59178.com/checi/
+
+def scrach_train_number(
